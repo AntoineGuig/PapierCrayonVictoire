7c7f82e149e10cdac9683e48ae63c488b38aadda
SamSeb <sebille.sam@gmail.com>
2025-01-21 09:18:56 +0100

    acceuil page header + footer

---
+++ pages/acceuil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Acceuil</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+</head>
+<body>
+<header class="header">
+    <?php include '../includes/header.html'; ?>
+</header>
+
+<footer class="footer">
+    <?php include '../includes/footer.html'; ?>
+</footer>
+</body>
+</html>
