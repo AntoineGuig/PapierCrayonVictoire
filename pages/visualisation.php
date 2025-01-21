db1d88f6d455f079a3786e05368730298f243f1e
Antoine Guillon <antoine.guillon28@gmail.com>
2025-01-21 16:27:07 +0100

    création fichiers visualisation dessins

---
+++ pages/visualisation.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Visualisation des dessins</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+</head>
+<body>
+<header class="header">
+    <?php include '../includes/header.php'; ?>
+</header>
+<div class="content-body">
+    <p>Bienvenue sur la page de visualisation des dessins></p>
+</div>
+<footer class="footer">
+    <?php include '../includes/footer.html'; ?>
+</footer>
+</body>
+</html>
