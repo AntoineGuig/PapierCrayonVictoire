8957da65eb1a6af23db37d7be1bae897f0e2a04d
Florian Morin <florianmorin6@gmail.com>
2025-01-21 12:45:24 +0100

    Add connexion session V3

--- config/connexion.php
+++ config/connexion.php
@@ -1,8 +1,9 @@
 <?php
 try {
-    $dsn = 'mysql:host=localhost;dbname=PapierCrayonVictoire;charset=utf8';
+    $dsn = 'mysql:host=137.116.243.181;dbname=PapierCrayonVictoire;charset=utf8';
     $username = 'db_etu';
     $password = 'N3twork!';
+    //$port = '5619';
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
