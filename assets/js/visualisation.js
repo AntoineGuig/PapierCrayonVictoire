df0efe9c9aa480036249dabc2959f314b82d70de
Antoine Guillon <antoine.guillon28@gmail.com>
2025-01-22 11:36:21 +0100

    ajout dessins concours

--- assets/js/visualisation.js
+++ assets/js/visualisation.js
@@ -1,9 +1,18 @@
 function voirMesDessins() {
   document.getElementById("listeMesDessins").style.display = "flex";
-  document.getElementById("listeDessinsClub").style.display = "none";
+  document.getElementById("listeConcours").style.display = "none";
 }
 
 function voirLesDessinsConcours() {
-  document.getElementById("listeDessinsClub").style.display = "flex";
+  document.getElementById("listeConcours").style.display = "flex";
   document.getElementById("listeMesDessins").style.display = "none";
+}
+
+function afficherDessinsConcours(numConcours) {
+  document.getElementsByName("dessinConcours").forEach(function (dessin) {
+    dessin.style.display = "none";
+  });
+  document.getElementsByName(numConcours).forEach(function (dessin) {
+    dessin.style.display = "flex";
+  });
 }
--- pages/visualisation.php
+++ pages/visualisation.php
@@ -29,11 +29,13 @@
         <div class="listeMesDessins" id="listeMesDessins" style="display:none;">
             <?php
             if (!empty($_SESSION['numUtilisateur'])) {
+
                 $sql = "SELECT * FROM dessin WHERE numCompetiteur = :idUser ORDER BY dateRemise desc;";
                 $stmt = $pdo->prepare($sql);
                 $stmt->bindParam(':idUser', $_SESSION['numUtilisateur']);
                 $stmt->execute();
                 $dessins = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
                 foreach ($dessins as $dessin) { ?>
                     <div class="dessin">
                         <img src="<?= $dessin['leDessin'] ?>" alt="dessin" class="imageDessin">
@@ -49,13 +51,42 @@
         </div>
 
 
-        <div class="listeDessinsClub" id="listeDessinsClub">
+        <div class="listeConcours" id="listeConcours" style="display:none;">
+            <?php
+            $sql = "SELECT * FROM Concours WHERE statut = 'terminé';";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            $concours = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($concours as $concour) { ?>
+                <div class="btnConcours">
+                    <button id="<?php echo $concour['numConcours'] ?>" onclick="afficherDessinsConcours(<?php echo $concour['numConcours'] ?>)">
+                        <?php echo htmlspecialchars($concour['theme']); ?>
+                    </button>
+                </div>
+            <?php } ?>
+
+            <div class="listeDessinsConcours" id="listeDessinsConcours" style="display:flex;">
+                <?php
+                $sql = "SELECT * FROM dessin WHERE numConcours = :numConcours ORDER BY dateRemise desc;";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(':numConcours', $concour['numConcours']);
+                $stmt->execute();
+                $dessinsConcours = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($dessinsConcours as $dessinConcours) { ?>
+                    <div class="dessinConcours" style="display:none;" name="<?php echo $numConcours ?>">
+                        <img src="<?= $dessinConcours['leDessin'] ?>" alt="dessin" class="imageDessin">
+                        <p><?= $dessinConcours['dateRemise'] ?></p>
+                        <p><?= $dessinConcours['commentaire'] ?></p>
+                    </div>
+                <?php } ?>
+
+            </div>
 
 
 
         </div>
-
-
     </div>
     <footer class="footer">
         <?php include '../includes/footer.html'; ?>
@@ -64,14 +95,3 @@
 
 </html>
 <script src="../assets/js/visualisation.js"></script>
-<script>
-    function voirMesDessins() {
-        document.getElementById("listeMesDessins").style.display = "flex";
-        document.getElementById("listeDessinsClub").style.display = "none";
-    }
-
-    function voirLesDessinsConcours() {
-        document.getElementById("listeDessinsClub").style.display = "flex";
-        document.getElementById("listeMesDessins").style.display = "none";
-    }
-</script>
