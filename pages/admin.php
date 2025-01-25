9e8a89f8441e292ee0ccf3a6875943c03147c77b
Antoine Guillon <antoine.guillon28@gmail.com>
2025-01-25 21:30:51 +0100

    fge

--- pages/admin.php
+++ pages/admin.php
@@ -19,11 +19,9 @@ if (!empty($nomClub) && !empty($adresse) && !empty($numTelephone) && !empty($nom
     $stmt->bindParam(':departement', $departement);
     $stmt->bindParam(':region', $region);
     $stmt->execute();
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
 } else {
     $error = "Veuillez remplir tous les champs.";
-} 
+}
 ?>
 
 <!DOCTYPE html>
@@ -37,45 +35,47 @@ if (!empty($nomClub) && !empty($adresse) && !empty($numTelephone) && !empty($nom
 </head>
 
 <body>
-<header class="header">
-    <?php include '../includes/header.php'; ?>
-</header>
+    <header class="header">
+        <?php include '../includes/header.php'; ?>
+    </header>
 
-<div class="content-body">
-    <p>Bienvenue sur la page admin</p>
-    <div id="recap-container"></div>
-    <h1>Création des clubs</h1>
-    <form action="" method="POST">
-        <label for="nomClub">Nom du club :</label>
-        <input type="text" id="nomClub" name="nomClub" required><br><br>
+    <div class="content-body">
+        <p>Bienvenue sur la page admin</p>
+        <div id="recap-container"></div>
+        <h1>Création des clubs</h1>
+        <form action="" method="POST">
+            <label for="nomClub">Nom du club :</label>
+            <input type="text" id="nomClub" name="nomClub" required><br><br>
 
-        <label for="adresse">Adresse :</label>
-        <input type="text" id="adresse" name="adresse" required><br><br>
+            <label for="adresse">Adresse :</label>
+            <input type="text" id="adresse" name="adresse" required><br><br>
 
-        <label for="numTelephone">Numéro de téléphone :</label>
-        <input type="text" id="numTelephone" name="numTelephone" required><br><br>
+            <label for="numTelephone">Numéro de téléphone :</label>
+            <input type="text" id="numTelephone" name="numTelephone" required><br><br>
 
-        <label for="nombreAdherents">Nombre d'adhérents :</label>
-        <input type="text" id="nombreAdherents" name="nombreAdherents" required><br><br>
+            <label for="nombreAdherents">Nombre d'adhérents :</label>
+            <input type="text" id="nombreAdherents" name="nombreAdherents" required><br><br>
 
-        <label for="ville">Ville :</label>
-        <input type="text" id="ville" name="ville" required><br><br>
+            <label for="ville">Ville :</label>
+            <input type="text" id="ville" name="ville" required><br><br>
 
-        <label for="departement">Département :</label>
-        <input type="text" id="departement" name="departement" required><br><br>
+            <label for="departement">Département :</label>
+            <input type="text" id="departement" name="departement" required><br><br>
 
-        <label for="region">Région :</label>
-        <input type="text" id="region" name="region" required><br><br>
+            <label for="region">Région :</label>
+            <input type="text" id="region" name="region" required><br><br>
 
-        <input type="submit" value="Envoyer"><br><br>
-    </form>
-    <script> formulaireAdmin(); </script>
+            <input type="submit" value="Envoyer"><br><br>
+        </form>
+        <script>
+            formulaireAdmin();
+        </script>
 
-</div>
-<footer class="footer">
-    <?php include '../includes/footer.html'; ?>
-</footer>
-<script src="../assets/js/admin.js"></script>
+    </div>
+    <footer class="footer">
+        <?php include '../includes/footer.html'; ?>
+    </footer>
+    <script src="../assets/js/admin.js"></script>
 </body>
 
 </html>
