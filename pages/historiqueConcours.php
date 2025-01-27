cd049bb2c2c08a9fbaea79df21854092fb53e062
SamSeb <sebille.sam@gmail.com>
2025-01-28 00:02:46 +0100

    rtyuiopjhgvcv

--- pages/historiqueConcours.php
+++ pages/historiqueConcours.php
@@ -1,1 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Concours en cours</title>
+    <link rel="stylesheet" href="../assets/css/futurConcours.css" />
+</head>
+<body>
+<header class="header">
+    <?php include '../includes/header.php'; ?>
+</header>
+<div class="content-body">
+    <?php include '../includes/historiqueConcours.php' ?>
+</div>
+<footer class="footer">
+    <?php include '../includes/footer.html'; ?>
+</footer>
+</body>
+</html>
