fc98b1978428920b3e572cc83b8b58ea296c002a
Antoine Guillon <antoine.guillon28@gmail.com>
2025-01-27 19:35:42 +0100

    création concours

--- pages/admin.php
+++ pages/admin.php
@@ -1,36 +1,6 @@
-<?php include('../config/connexion.php');
-$nomClub = trim($_POST['nomClub']);
-$adresse = trim($_POST['adresse']);
-$numTelephone = trim($_POST['numTelephone']);
-$nombreAdherents = trim($_POST['nombreAdherents']);
-$ville = trim($_POST['ville']);
-$departement = trim($_POST['departement']);
-$region = trim($_POST['region']);
-
-if (!empty($nomClub) && !empty($adresse) && !empty($numTelephone) && !empty($nombreAdherents) && !empty($ville) && !empty($departement) && !empty($region)) {
-    $sql = "INSERT INTO Club(numClub, nomClub, adresse, numTelephone, nombreAdherents, ville, departement, region, dateDeCreation) VALUES
-    (NULL, :nomClub, :adresse, :numTelephone, :nombreAdherents, :ville, :departement, :region, CURRENT_DATE()";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':nomClub', $nomClub);
-    $stmt->bindParam(':adresse', $adresse);
-    $stmt->bindParam(':numTelephone', $numTelephone);
-    $stmt->bindParam(':nombreAdherents', $nombreAdherents);
-    $stmt->bindParam(':ville', $ville);
-    $stmt->bindParam(':departement', $departement);
-    $stmt->bindParam(':region', $region);
-    $stmt->execute();
-} else {
-    $error = "Veuillez remplir tous les champs.";
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <title>Visualisation des dessins</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+    <title>Administration concours</title>
     <link rel="stylesheet" href="../assets/css/admin.css" />
 </head>
 
@@ -43,39 +13,24 @@ if (!empty($nomClub) && !empty($adresse) && !empty($numTelephone) && !empty($nom
         <p>Bienvenue sur la page admin</p>
         <div id="recap-container"></div>
         <h1>Création des clubs</h1>
-        <form action="" method="POST">
-            <label for="nomClub">Nom du club :</label>
-            <input type="text" id="nomClub" name="nomClub" required><br><br>
+        <form action="creerConcours.php" method="POST">
+            <label for="theme">Theme :</label>
+            <input type="text" name="theme" required><br><br>
 
-            <label for="adresse">Adresse :</label>
-            <input type="text" id="adresse" name="adresse" required><br><br>
+            <label for="dateDebut">Date début concours :</label>
+            <input type="date" name="dateDebut" required><br><br>
 
-            <label for="numTelephone">Numéro de téléphone :</label>
-            <input type="text" id="numTelephone" name="numTelephone" required><br><br>
-
-            <label for="nombreAdherents">Nombre d'adhérents :</label>
-            <input type="text" id="nombreAdherents" name="nombreAdherents" required><br><br>
-
-            <label for="ville">Ville :</label>
-            <input type="text" id="ville" name="ville" required><br><br>
-
-            <label for="departement">Département :</label>
-            <input type="text" id="departement" name="departement" required><br><br>
-
-            <label for="region">Région :</label>
-            <input type="text" id="region" name="region" required><br><br>
-
+            <label for="dateFin">Date début fin :</label>
+            <input type="date" name="dateFin" required />
+            <br><br>
             <input type="submit" value="Envoyer"><br><br>
         </form>
-        <script>
-            formulaireAdmin();
-        </script>
+
 
     </div>
     <footer class="footer">
         <?php include '../includes/footer.html'; ?>
     </footer>
-    <script src="../assets/js/admin.js"></script>
 </body>
 
 </html>
---
+++ pages/creerConcours.php
@@ -0,0 +1,41 @@
+<?php
+include('../config/connexion.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $theme = htmlspecialchars(trim($_POST["theme"]));
+    $dateDebut = htmlspecialchars(trim($_POST["dateDebut"]));
+    $dateFin = htmlspecialchars(trim($_POST["dateFin"]));
+
+
+    // Validation de base
+    if (!empty($theme) && !empty($dateDebut) && !empty($dateFin)) {
+        $stmt = $pdo->prepare("INSERT INTO Concours (theme, dateDebut, dateFin, statut) VALUES (?, ?, ?, 'pas commencé');");
+        if ($stmt->execute([$theme, $dateDebut, $dateFin])) {
+            $message = "Création du concours !";
+        } else {
+            $message = "Une erreur est survenue lors de la création.";
+        }
+    }
+} else {
+    $message = "Tous les champs sont obligatoires.";
+}
+
+?>
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/register.css" />
+    <title>Inscription</title>
+
+</head>
+
+<body>
+    <?php if (isset($message)): ?>
+        <p><?php echo $message; ?></p>
+    <?php endif; ?>
+    <a href="admin.php">Retour à la page admin.</a>
+</body>
+
+</html>
