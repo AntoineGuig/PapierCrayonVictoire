cffbcc081154322ad25793dd042efaae2cad04ea
Antoine Guillon <antoine.guillon28@gmail.com>
2025-01-27 18:00:15 +0100

    fixe errreur que sam n'a pas résolu

--- config/register.php
+++ config/register.php
@@ -3,28 +3,28 @@ include('connexion.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données du formulaire
-    $name = htmlspecialchars(trim($_POST["name"]));
-    $lastname = htmlspecialchars(trim($_POST["lastname"]));
-    $email = htmlspecialchars(trim($_POST["login"]));
-    $address = htmlspecialchars(trim($_POST["address"]));
+    $prenom = htmlspecialchars(trim($_POST["prenom"]));
+    $nom = htmlspecialchars(trim($_POST["nom"]));
+    $login = htmlspecialchars(trim($_POST["login"]));
+    $adresse = htmlspecialchars(trim($_POST["adresse"]));
     $password = htmlspecialchars(trim($_POST["password"]));
-    $birthdate = htmlspecialchars(trim($_POST["birth-date"]));
-    $gender = htmlspecialchars(trim($_POST["gender"]));
+    $dateNaissance = htmlspecialchars(trim($_POST["dateNaissance"]));
+    $sexe = htmlspecialchars($_POST["sexe"]);
 
     // Validation de base
-    if (!empty($name) && !empty($lastname) && !empty($email) && !empty($address) && !empty($password) && !empty($birthdate) && !empty($gender)) {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!empty($prenom) && !empty($nom) && !empty($login) && !empty($adresse) && !empty($password) && !empty($dateNaissance) && !empty($sexe)) {
+        if (filter_var($login, FILTER_VALIDATE_EMAIL)) {
             $hash_password = hash('sha256', $password);
 
             // Vérifier si l'utilisateur existe déjà
             $stmt = $pdo->prepare("SELECT * FROM Utilisateur WHERE login = ?");
-            $stmt->execute([$email]);
+            $stmt->execute([$login]);
             if ($stmt->rowCount() > 0) {
                 $message = "Cet email est déjà utilisé.";
             } else {
                 // Insérer dans la base de données
-                $stmt = $pdo->prepare("INSERT INTO Utilisateur (nom, prenom, adresse, login, motDePasse, dateDeNaissance, sexe) VALUES (?, ?, ?, ?, ?, ?, ?)");
-                if ($stmt->execute([$lastname, $name, $address, $email, $hash_password, $birthdate, $gender])) {
+                $stmt = $pdo->prepare("INSERT INTO Utilisateur (nom, prenom, adresse, login, motDePasse,numClub, dateDeNaissance, sexe) VALUES (?, ?, ?, ?, ?,NULL, ?, ?);");
+                if ($stmt->execute([$nom, $prenom, $adresse, $login, $hash_password, $dateNaissance, $sexe])) {
                     $message = "Inscription réussie !";
                 } else {
                     $message = "Une erreur est survenue lors de l'inscription.";
@@ -37,4 +37,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $message = "Tous les champs sont obligatoires.";
     }
 }
-?>
