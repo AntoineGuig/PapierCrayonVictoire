acd68da7d462976a2edaae216d6fb9a6a4e28c8b
SamSeb <sebille.sam@gmail.com>
2025-01-28 11:24:16 +0100

    rtyuiopjhgvcv

--- includes/header.php
+++ includes/header.php
@@ -2,103 +2,96 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CodingLab Navbar</title>
-  <link rel="stylesheet" href="../assets/css/header.css">
-  <link rel="stylesheet" href="../assets/css/global.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CodingLab Navbar</title>
+    <link rel="stylesheet" href="../assets/css/header.css">
+    <link rel="stylesheet" href="../assets/css/global.css">
 </head>
 
 <body>
-  <?php
-  session_start();
-  if (empty($_SESSION['login'])) {
+<?php
+session_start();
+if (empty($_SESSION['login'])) {
     header("Location:/");
     die();
-  }
-  include("../config/connexion.php");
-  ?>
+}
+include("../config/connexion.php");
+?>
 
-  <nav>
+<nav>
     <div class="navbar">
-      <i class='bx bx-menu'></i>
-      <div class="logo"><a href="accueil.php">
-          <img src="../assets/img/Logo_sans_fond.png" alt="logo">
-        </a>
-      </div>
-      <div class="nav-links">
-        <ul class="links">
-          <li><a href="accueil.php">Accueil</a></li>
-          <li>
-            <a href="#">Les concours</a>
-            <i class='bx bxs-chevron-down htmlcss-arrow arrow'></i>
-            <ul class="htmlCss-sub-menu sub-menu">
-              <li><a href="concoursEnCours.php">Concours en cours</a></li>
-              <li><a href="futurConcours.php">Futur concours</a></li>
-              <li><a href="historiqueConcours.php">Historique des concours</a></li>
+        <i class='bx bx-menu'></i>
+        <div class="logo"><a href="accueil.php">
+                <img src="../assets/img/Logo_sans_fond.png" alt="logo">
+            </a>
+        </div>
+        <div class="nav-links">
+            <ul class="links">
+                <li><a href="accueil.php">Accueil</a></li>
+                <li>
+                    <a href="#">Les concours</a>
+                    <i class='bx bxs-chevron-down htmlcss-arrow arrow'></i>
+                    <ul class="htmlCss-sub-menu sub-menu">
+                        <li><a href="concoursEnCours.php">Concours en cours</a></li>
+                        <li><a href="futurConcours.php">Futur concours</a></li>
+                        <li><a href="historiqueConcours.php">Historique des concours</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Les dessins</a>
+                    <i class='bx bxs-chevron-down js-arrow arrow'></i>
+                    <ul class="js-sub-menu sub-menu">
+                        <li><a href="deposerDessin.php">Déposer les dessins</a></li>
+                        <li><a href="visualisation.php">Visualiser les dessins</a></li>
+                    </ul>
+                </li>
+                <li><a href="aProposDeNous.php">A propos de nous</a></li>
             </ul>
-          </li>
-          <li>
-            <a href="#">Les clubs</a>
-            <i class='bx bxs-chevron-down js-arrow arrow'></i>
-            <ul class="js-sub-menu sub-menu">
-              <li><a href="#">Informations clubs</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">Les dessins</a>
-            <i class='bx bxs-chevron-down js-arrow arrow'></i>
-            <ul class="js-sub-menu sub-menu">
-              <li><a href="deposerDessin.php">Déposer les dessins</a></li>
-              <li><a href="visualisation.php">Visualiser les dessins</a></li>
-            </ul>
-          </li>
-          <li><a href="#">A propos de nous</a></li>
-        </ul>
-      </div>
-      <?php
-      if (isset($_POST['deconnexion'])) {
-        session_destroy();
-        header("Location:/");
-      }
+        </div>
+        <?php
+        if (isset($_POST['deconnexion'])) {
+            session_destroy();
+            header("Location:/");
+        }
 
-      if (isset($_POST['admin'])) {
-        header("Location:../pages/admin.php");
-      }
-      ?>
-      <span class="deconnexion">
+        if (isset($_POST['admin'])) {
+            header("Location:../pages/admin.php");
+        }
+        ?>
+        <span class="deconnexion">
         <?php echo "<p>Bonjour " . $_SESSION['prenom'] . " ! </p>  <form method=\"post\"> <input type=\"submit\" name=\"deconnexion\" value=\"deconnexion\"/> </form> "
         ?>
       </span>
-      <div class="admin">
-        <?php
-        // Vérifiez que la connexion PDO existe
-        global $pdo;
+        <div class="admin">
+            <?php
+            // Vérifiez que la connexion PDO existe
+            global $pdo;
 
-        try {
-          $sql = "SELECT login from Utilisateur inner JOIN Administrateur on Administrateur.numAdmin = Utilisateur.numUtilisateur;";
-          $stmt = $pdo->prepare($sql);
-          $stmt->execute();
+            try {
+                $sql = "SELECT login from Utilisateur inner JOIN Administrateur on Administrateur.numAdmin = Utilisateur.numUtilisateur;";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute();
 
-          // Récupérez toutes les données si nécessaire
-          $loginAdmin = $stmt->fetch(PDO::FETCH_ASSOC);
+                // Récupérez toutes les données si nécessaire
+                $loginAdmin = $stmt->fetch(PDO::FETCH_ASSOC);
 
-          // Vérifiez si l'utilisateur connecté est un administrateur
-          if (isset($_SESSION['login']) && $loginAdmin && $loginAdmin['login'] == $_SESSION['login']) {
-            echo "<form method=\"post\"> <input type=\"submit\" name=\"admin\" value=\"admin\"/> </form>";
-          }
-        } catch (PDOException $e) {
-          echo "Erreur : " . $e->getMessage();
-        }
-        ?>
-      </div>
+                // Vérifiez si l'utilisateur connecté est un administrateur
+                if (isset($_SESSION['login']) && $loginAdmin && $loginAdmin['login'] == $_SESSION['login']) {
+                    echo "<form method=\"post\"> <input type=\"submit\" name=\"admin\" value=\"admin\"/> </form>";
+                }
+            } catch (PDOException $e) {
+                echo "Erreur : " . $e->getMessage();
+            }
+            ?>
+        </div>
 
-
-    </div>
 
     </div>
+
     </div>
-  </nav>
+    </div>
+</nav>
 </body>
 
 </html>
--- pages/admin.php
+++ pages/admin.php
@@ -1,36 +1,36 @@
 <head>
     <meta charset="UTF-8">
     <title>Administration concours</title>
-    <link rel="stylesheet" href="../assets/css/admin.css" />
+    <link rel="stylesheet" href="../assets/css/admin.css"/>
 </head>
 
 <body>
-    <header class="header">
-        <?php include '../includes/header.php'; ?>
-    </header>
+<header class="header">
+    <?php include '../includes/header.php'; ?>
+</header>
 
-    <div class="content-body align_center">
-        <p>Bienvenue sur la page admin</p>
-        <div id="recap-container"></div>
-        <h1>Création des clubs</h1>
-        <form action="creerConcours.php" method="POST">
-            <label for="theme">Theme :</label>
-            <input type="text" name="theme" required><br><br>
+<div class="content-body align_center">
+    <p>Bienvenue sur la page admin</p>
+    <div id="recap-container"></div>
+    <h1>Création des concours</h1>
+    <form action="creerConcours.php" method="POST">
+        <label for="theme">Thème :</label>
+        <input type="text" name="theme" required><br><br>
 
-            <label for="dateDebut">Date début concours :</label>
-            <input type="date" name="dateDebut" required><br><br>
+        <label for="dateDebut">Date début concours :</label>
+        <input type="date" name="dateDebut" required><br><br>
 
-            <label for="dateFin">Date début fin :</label>
-            <input type="date" name="dateFin" required />
-            <br><br>
-            <input type="submit" value="Envoyer"><br><br>
-        </form>
+        <label for="dateFin">Date fin concours :</label>
+        <input type="date" name="dateFin" required/>
+        <br><br>
+        <input type="submit" value="Envoyer"><br><br>
+    </form>
 
 
-    </div>
-    <footer class="footer">
-        <?php include '../includes/footer.html'; ?>
-    </footer>
+</div>
+<footer class="footer">
+    <?php include '../includes/footer.html'; ?>
+</footer>
 </body>
 
 </html>
